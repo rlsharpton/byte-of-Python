a25637af3195e9d2674dc3e6c7365507f3742bd3
rlsharpton <robin@robinls.net>
2015-08-10 09:57:00 -0500

    quick file

---
+++ using_tuple.py
@@ -0,0 +1,9 @@
+__author__ = 'rls'
+
+zoo = ('python', 'elephant', 'penguin')
+print("Number of animals in the zoo is", len(zoo))
+
+new_zoo = 'monkey', 'camel', zoo
+
+print("Number of cages in the new zoo is", len(new_zoo))
+
