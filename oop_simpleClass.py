1dc0b8522b9a44ce73214babc82c583fb61c98bb
rlsharpton <robin@robinls.net>
2015-08-10 11:07:53 -0500

    quick file

--- oop_simpleClass.py
+++ oop_simpleClass.py
@@ -1,9 +1,11 @@
 __author__ = 'rls'
 
 class Person:
+    def __init__(self, name):
+        self.name = name
     def say_hi(self):
-        print("Hello, how are you?")
+        print("Hello, how are you? My name is ", self.name)
 
-p = Person()
+p = Person('Robin')
 print(p)
 p.say_hi()
