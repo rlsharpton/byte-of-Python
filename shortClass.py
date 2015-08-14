115765bbe8c1fc27f70b670915c856b5298587e3
rlsharpton <robin@robinls.net>
2015-08-14 10:57:41 -0500

    New repo see if it updates stats.

--- shortClass.py
+++ shortClass.py
@@ -10,7 +10,7 @@ class Employee:
       Employee.empCount += 1
 
    def displayCount(self):
-     print("Total Employee %d" % Employee.empCount)
+     print("Total number of Employee %d" % Employee.empCount)
 
    def displayEmployee(self):
       print("Name : ", self.name,  ", Salary: ", self.salary)
