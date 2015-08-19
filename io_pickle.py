b310db3dd5174064cd874f472ad18c95f4aed6a8
rlsharpton <robin@robinls.net>
2015-08-19 10:08:27 -0500

    This is in a branch of the master branch on the byte-of-python repo

---
+++ io_pickle.py
@@ -0,0 +1,26 @@
+__author__ = 'rls'
+
+import pickle
+
+# The name of the file where we will store the object
+shoplistfile = 'shoplist.data'
+# The list of things
+shoplist = ['apple', 'mango', 'carrot']
+
+# Write to the file
+f = open(shoplistfile, 'wb')
+# Dump the object to a file
+pickle.dump(shoplist, f)
+f.close()
+
+# Destroy the shoplist variable
+del shoplist
+
+# Read back from the storage
+f = open(shoplistfile, 'rb')
+# Load the object from the file
+storedlist = pickle.load(f)
+print(storedlist)
+
+
+__version__ = 0.1
